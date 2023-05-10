8b5dd961adc8481fd79cd5ec974cb3c8eec66075
Paweł <113582471+666godfist@users.noreply.github.com>
2023-05-10 15:13:56 +0200

    Add files via upload

---
+++ lista.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista przyjaciół</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+    <div class="baner">
+        <h1> Portal społecznościowy-moje konto</h1>
+    </div>
+
+    <div class="glowny">
+        <h2> Moje zainteresowania </h2>
+        <ul>
+            <li>muzyka</li>
+            <li>film</li>
+            <li>komputery</li>
+        </ul>
+        <h2>Moi znajomi</h2>
+        <?php
+            $polaczenie = mysqli_connect('localhost', 'root', '', 'dane');
+            $zapytanie = "SELECT imie, nazwisko, opis, zdjecie FROM osoby WHERE Hobby_id IN (1,2,6)";
+            
+            $wynik = mysqli_query($polaczenie, $zapytanie);
+            while($wiersz = mysqli_fetch_array($wynik)){
+                echo "<div class='zdjecie'><img src='$wiersz[3]' alt='przyjaciel'></div>";
+                echo "<div class='opis'>
+                        <h3>$wiersz[0] $wiersz[1]</h3>
+                        <p>Ostatni wpis: $wiersz[2]</p>
+                        </div>";
+                echo "<div class='linia'><hr></div>";
+            }
+            mysqli_close($polaczenie);
+        ?>
+    </div>
+    <div class="stopka1">
+        Stronę wykonał: Paweł Kozłowski
+    </div>
+    <div class="stopka2">
+        <a href="ja@portal.pl">napisz do mnie</a>
+    </div>
+</body>
+</html>
